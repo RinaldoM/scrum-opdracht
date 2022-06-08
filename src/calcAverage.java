5fc2f2a05bc32b31fc62d2a5e5bd0358fe59be41
rinaldo <menichettir@hotmail.com>
2022-06-08 21:00:28 +0200

    L - Gemiddelde berekenen (prio 1) - done

--- src/calcAverage.java
+++ src/calcAverage.java
@@ -3,6 +3,10 @@ import java.util.List;
 public class calcAverage implements ICalculator{
     @Override
     public double calculate(List<Double> values) {
-        return 0;
+        double sum = 0;
+        for(int i=0; i<values.size(); i++){
+            sum+=values.get(i);
+        }
+        return sum/ values.size();
     }
 }
